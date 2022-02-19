7c4dfb514fd24169cf373c607be56c68061b89f3
sbritobreno <breno.silvabrito'hotmail.com>
2022-02-19 19:11:30 +0000

    Creating the second activity

--- app/src/main/java/com/brito/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/brito/myfirstandroidapp/MainActivity.kt
@@ -1,12 +1,20 @@
  package com.brito.myfirstandroidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+ const val MAIN_ACT= "mainAct"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btn = findViewById<Button>(R.id.skills)
+        btn.setOnClickListener {
+            val intent = Intent(this, SkillsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
